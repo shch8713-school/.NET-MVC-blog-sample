2d5c48b8349f39ccda752a7d6199a6f3283bffdf
shch8713 <47016159+shch8713@users.noreply.github.com>
2024-01-08 21:13:56 +0800

    Delete Documents/ERP/HRMS directory

--- Documents/ERP/HRMS/HRMS.solution/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HRMS.solution.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
